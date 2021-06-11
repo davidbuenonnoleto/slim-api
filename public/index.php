f2eaa548d28bb8f1190acc08db7dd8dbc01ef982
David Noleto <dr.dave2@gmail.com>
2021-06-10 22:42:37 -0700

    add middleware & schema

--- public/index.php
+++ public/index.php
@@ -13,40 +13,52 @@ require __DIR__ . '/../vendor/autoload.php';
 */
 $app = AppFactory::create();
 
+
+/*
+* Middleware for authentication
+*/
+$authMiddleware = function (Request $request, RequestHandler $handler) {
+    $response = $handler->handle($request);
+    $response->getBody()->write(' Authenticated ');
+
+    return $response;
+};
+
 /**
  * API routes
 */
 $app->get('/', function (Request $request, Response $response, array $args) {
+
     $response->getBody()->write("API using Slim Micro Framework");
     return $response->withHeader('Content-Type', 'application/json');
 });
 
-$app->group('/drivers/[{id:[0-9]+}]', function (RouteCollectorProxy $group) {
+$app->group('/drivers[/{id:[0-9]+}]', function (RouteCollectorProxy $group) {
     $group->map(['GET', 'DELETE', 'PATCH', 'PUT'], '', function ($request, $response, array $args) {
         
         $response->getBody()->write('david, joao, bastiao, jonas');
         return $response->withHeader('Content-Type', 'application/json');
         
     });   
-});
+})->add($authMiddleware);
 
-$app->group('/routes/[{id:[0-9]+}]', function (RouteCollectorProxy $group) {
+$app->group('/routes[/{id:[0-9]+}]', function (RouteCollectorProxy $group) {
     $group->map(['GET', 'DELETE', 'PATCH', 'PUT'], '', function ($request, $response, array $args) {
         
         $response->getBody()->write('s10570a, s10570b, s10570c, s10570d');
         return $response->withHeader('Content-Type', 'application/json');
         
     });   
-});
+})->add($authMiddleware);
 
-$app->group('/packages/[{id:[0-9]+}]', function (RouteCollectorProxy $group) {
+$app->group('/packages[/{id:[0-9]+}]', function (RouteCollectorProxy $group) {
     $group->map(['GET', 'DELETE', 'PATCH', 'PUT'], '', function ($request, $response, array $args) {
         
         $response->getBody()->write('22, 45, 68, 72');
         return $response->withHeader('Content-Type', 'application/json');
         
     });   
-});
+})->add($authMiddleware);
 
 // Run app
 $app->run();
